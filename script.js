795e319495acb639e8eaa3182c8de2443a8e900f
Esa Dewa Dalla Kharisma <hello.esadewa@gmail.com>
2024-07-22 15:17:56 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example: Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
